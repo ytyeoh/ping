1f44427bec394fc46fdae77e806cc30e16784d37
ytyeoh <ytyeoh@users.noreply.github.com>
2022-06-07 17:30:37 +0800

    doen ecosystem

--- static/js/flowbite.js
+++ static/js/flowbite.js
@@ -3061,8 +3061,8 @@ function tabs_createClass(Constructor, protoProps, staticProps) { if (protoProps
 
 var tabs_Default = {
   defaultTabId: null,
-  activeClasses: 'main-bg-ud text-white hover:text-grey-200 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500',
-  inactiveClasses: 'dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
+  activeClasses: 'main-bg-ud text-white hover:text-grey-200 dark:text-blue-500 dark:hover:text-blue-500 border-white dark:border-white',
+  inactiveClasses: 'border-2 border-black dark:border-transparent text-black hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
   onShow: function onShow() {}
 };
 
